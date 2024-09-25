5bcd40c79f2ad16e09a3e45a0c870c43d409da98
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2024-09-24 22:20:28 -0700

    Add: server sorting mimic. By price & name.

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 var bodyParser = require("body-parser");
 
-const data = require('./data');
-
+const data = require("./data");
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => { 
-    console.log(req.query);
-    const {price, id} = req.query;
-    console.log(price, id);
-// res.send("id: " + req.query.id);
-res.send({price, id});
-
+app.get("/items", (req, res) => {
+  const { sortBy } = req.query;
+  //   console.log(req.query);
+  //   console.log(sortBy);
+  if (sortBy === "price") {
+    data.sort((a, b) => a.price - b.price);
+    res.send(data);
+  } else if (sortBy === "-price") {
+    data.sort((a, b) => b.price - a.price);
+    res.send(data);
+  } else if (sortBy === "name") {
+    data.sort((a, b) => {
+      //   a.title - b.title;
+      const nameA = a.title.toUpperCase();
+      const nameB = b.title.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    res.send(data);
+  } else {
+    //   console.log("Hi");
+    data.sort((a, b) => a.id - b.id);
+    res.send(data);
+  }
 });
-
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
