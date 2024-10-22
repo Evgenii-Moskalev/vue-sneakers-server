2709d0e107719a24dd6520406b63556a132c9004
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2024-10-21 22:18:46 -0700

    Feat: removing favorite items from backend using nanoid

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 let bodyParser = require("body-parser");
-const { items: data, favoriteItems } = require("./data");
+const { nanoid } = require("nanoid");
+const { items, favoriteItems } = require("./data");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const checkMatch = (data, title) => {
-  let result = data.filter((element) => {
+const checkMatch = (items, title) => {
+  let result = items.filter((element) => {
     return element.title.toLowerCase().includes(title.toLowerCase());
   });
 
   if (result.length != 0) {
     return result;
   }
-  //   else return data;
+  //   else return items;
   console.log(checkMatch);
 };
 
@@ -25,13 +26,13 @@ app.get("/items", (req, res) => {
   //   console.log(sortBy, title);
 
   if (sortBy === "price") {
-    data.sort((a, b) => a.price - b.price);
-    res.send(checkMatch(data, title));
+    items.sort((a, b) => a.price - b.price);
+    res.send(checkMatch(items, title));
   } else if (sortBy === "-price") {
-    data.sort((a, b) => b.price - a.price);
-    res.send(checkMatch(data, title));
+    items.sort((a, b) => b.price - a.price);
+    res.send(checkMatch(items, title));
   } else if (sortBy === "name") {
-    data.sort((a, b) => {
+    items.sort((a, b) => {
       //   a.title - b.title;
       const nameA = a.title.toUpperCase();
       const nameB = b.title.toUpperCase();
@@ -44,15 +45,14 @@ app.get("/items", (req, res) => {
       return 0;
     });
 
-    res.send(checkMatch(data, title));
+    res.send(checkMatch(items, title));
   } else {
     //   console.log("Hi");
-    data.sort((a, b) => a.id - b.id);
+    items.sort((a, b) => a.id - b.id);
 
-    res.send(checkMatch(data, title));
+    res.send(checkMatch(items, title));
   }
 });
-
 
 app.get("/favorites", (req, res) => {
   res.send(favoriteItems);
@@ -62,8 +62,27 @@ app.post("/favorites", (req, res) => {
   // console.log(req.body);
 
   const { parentId } = req.body;
-  favoriteItems.push({ id: favoriteItems.length + 1, parentId: parentId });
-  res.send(favoriteItems);
+  const favoriteItem = {
+    id: nanoid(),
+    parentId: parentId,
+  };
+  favoriteItems.push(favoriteItem);
+  res.send(favoriteItem);
+});
+
+app.delete("/favorites/:id", (req, res) => {
+  const favoriteIdSent = req.params.id;
+  // console.log(favoriteIdSent);
+  favoriteItems.forEach((item) => {
+    if (item.id === favoriteIdSent) {
+      const findIndex = favoriteItems.indexOf(item);
+      // console.log(findIndex);
+      favoriteItems.splice(findIndex, 1);
+      return;
+    }
+  });
+
+  res.send("Item Deleted");
 });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
